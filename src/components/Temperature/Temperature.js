0d3e11d3ebbbda8c54f16ed46e6b7139e310db03
maricd7 <dejanmarci259@gmail.com>
2023-12-21 14:16:57 +0100

    connecting data from 2 apis

--- src/components/Temperature/Temperature.js
+++ src/components/Temperature/Temperature.js
@@ -3,14 +3,20 @@ import { Search } from "../Search";
 
 export const Temperature = () => {
   const apiKey = process.env.REACT_APP_API_KEY;
-  //    const URL_WEATHER = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
-  const URL_LOCATION = `https://api.openweathermap.org/geo/1.0/direct?q='London'&limit=5&appid=${apiKey}`;
   const [location, setLocation] = useState("");
+  const [lat,setLat] = useState(null);
+  const [lon,setLon] = useState(null);
 
+  const URL_WEATHER = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+  const URL_LOCATION = `https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${apiKey}`;
+
+// Handling location
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(URL_LOCATION);
-      // Handle the response here
+      const data = await response.json(); 
+      setLon(data[0]?.lon)
+      setLat(data[0]?.lat)
     };
     fetchData();
   }, [URL_LOCATION, apiKey]);
@@ -22,6 +28,17 @@ export const Temperature = () => {
   function searchCity(city) {
     console.log(city);
   }
+// Handling weather
+useEffect(() => {
+  const fetchWeather = async () => {
+    if (lat !== null && lon !== null) {
+      const weatherRes = await fetch(URL_WEATHER);
+      const data = await weatherRes.json();
+      console.log(data);
+    }
+  };
+  fetchWeather();
+}, [URL_WEATHER, lat, lon]);
 
   return (
     <div>
