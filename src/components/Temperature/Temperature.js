7eebbccd89a52e8e45862c84a18ae2b2020ff94e
maricd7 <dejanmarci259@gmail.com>
2023-12-21 11:52:42 +0100

    fixing env

--- src/components/Temperature/Temperature.js
+++ src/components/Temperature/Temperature.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Temperature = () => {
     const apiKey = process.env.REACT_APP_API_KEY;
-//    const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    console.log(apiKey)
+//    const URL_WEATHER = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    const URL_LOCATION = `https://api.openweathermap.org/geo/1.0/direct?q='London'&limit=5&appid=${apiKey}`;
+    const [location, setLocation] = useState(''); 
+
+    useEffect(()=>{
+        fetch(URL_LOCATION)
+    },[])
     return (
     <h1>Temperature</h1>
   )
