521b221b3cb0019a3d5c618ee3509cdde6c2f387
unknown <dejanmaric259@gmail.com>
2023-12-22 22:55:52 +0100

    finish

--- src/components/UI/City/City.js
+++ src/components/UI/City/City.js
@@ -6,7 +6,7 @@ export const City = ({city, weatherStatusText}) => {
         {city && (
             <div>
                 <h2 className="text-4xl font-semibold my-2 text-slate-700">{city}</h2>
-                <h4 className="text-2xl  my-2 text-slate-400">{weatherStatusText}</h4>
+                <h4 className="text-2xl  my-2 text-slate-400 text-center">{weatherStatusText}</h4>
             </div>
         )}
     </div>
